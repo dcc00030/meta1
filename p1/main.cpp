eb9eddc0e81543434af9f1adfc8036160d593881
dcasquel <dcasquel@dcasquel95>
2016-10-06 12:47:59 +0200

    creacion de proyecto

---
+++ p1/main.cpp
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: alumno
+ *
+ * Created on 3 de octubre de 2016, 10:55
+ */
+
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+//Toma un buffer de chars y lo pasa a int
+int catoi (char buff []){
+    string s = (string) buff;
+    return stoi(s);
+}
+
+int main(int argc, char** argv) {
+    
+    ifstream file ("scpe1.txt");
+    char buffer[128];
+    file.getline(buffer,256,' '); //Elimina espacio inicial
+    
+    file.getline(buffer,256,' '); //Obtengo num filas
+    int nFilas = catoi(buffer);
+    
+    file.getline(buffer,256,' '); //Obtengo num colunmnas
+    int nColum = catoi(buffer);
+    
+    int matriz[nFilas+1][nColum]; //Añado una fila para el coste
+    
+    if (file.is_open()){
+        
+        while (!file.eof()){
+            
+            file.getline(buffer,256,' ');
+
+            string test = (string)buffer;
+
+            int num = stoi(test);
+
+            file.close();
+        }
+
+        
+    }
+
+    return 0;
+}
