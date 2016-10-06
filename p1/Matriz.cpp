c8ce400a520f0e2d24b482ec1a3170aab2d5d862
dcasquel <dcasquel@dcasquel95>
2016-10-06 20:57:12 +0200

    funcionamiento correcto de la matriz (constructor e insercion)

--- p1/Matriz.cpp
+++ p1/Matriz.cpp
@@ -15,52 +15,54 @@
 #include <stdexcept>
 using namespace std;
 
-Matriz::Matriz(int nF, int nC){
+Matriz::Matriz(int nF, int nC) {
     if (nF <= 0) throw invalid_argument("Number of rows must be grater than 0");
     if (nC <= 0) throw invalid_argument("Number of columns must be grater than 0");
-    
+
     fil = nF;
     col = nC;
     m = new int*[fil];
-    for (int i = 0; i < fil; i++){
-        m[fil] = new int[col];
-        m[fil][col] = -1;
+    for (int i = 0; i < fil; i++) {
+            m[i] = new int[col];
+            for(int j = 0; j < col; j++){
+                m[i][j] = -1;
+            }
+        }
+
     }
-    
-    
-}
 
 //Devuelve -1 si no existe
-int Matriz::at(int x, int y){
-    if ( x < col && y < fil ) return m[x][y];
+
+int Matriz::at(int x, int y) {
+    if (x < col && y < fil) return m[x][y];
     else return -1;
 }
 
-bool Matriz::eliminar(int x, int y){
-    if ( x < col && y < fil ){
+bool Matriz::eliminar(int x, int y) {
+    if (x < col && y < fil) {
         m[x][y] = -1;
         return true;
     }
     return false;
-    
+
 }
 
 //No sobreescribe. Si la posición está ocupada o no existe, devuelve falso
-bool Matriz::insertar(int x, int y, int dato){
-    if ( x < col && y < fil ){
-        if (m[x][y] != -1){
+
+bool Matriz::insertar(int x, int y, int dato) {
+    if (x < col && y < fil) {
+        if (m[x][y] == -1) {
             m[x][y] = dato;
             return true;
         }
         return false;
-        
+
     }
-    
     else return false;
 }
-        
-Matriz::~Matriz(){
-    for (int i = 0; i < fil; i++){
+
+Matriz::~Matriz() {
+    for (int i = 0; i < fil; i++) {
         delete [] m[i];
     }
     delete []m;
--- p1/main.cpp
+++ p1/main.cpp
@@ -21,44 +21,83 @@
 using namespace std;
 
 //Toma un buffer de chars y lo pasa a int
-int catoi (char buff []){
+
+int catoi(char buff []) {
     string s = (string) buff;
     return stoi(s);
 }
 
 int main(int argc, char** argv) {
-    
-//    ifstream file ("scpe1.txt");
-//    char buffer[128];
-//    file.getline(buffer,256,' '); //Elimina espacio inicial
-//    
-//    file.getline(buffer,256,' '); //Obtengo num filas
-//    int nFilas = catoi(buffer);
-//    
-//    file.getline(buffer,256,' '); //Obtengo num colunmnas
-//    int nColum = catoi(buffer);
-//    
-//    int matriz[nFilas+1][nColum]; //Añado una fila para el coste
-//    
-//    if (file.is_open()){
-//        
-//        while (!file.eof()){
-//            
-//            file.getline(buffer,256,' ');
-//
-//            string test = (string)buffer;
-//
-//            int num = stoi(test);
-//
-//            file.close();
-//        }
-//
-//        
-//    }
+
+    //    ifstream file ("scpe1.txt");
+    //    char buffer[128];
+    //    file.getline(buffer,256,' '); //Elimina espacio inicial
+    //    
+    //    file.getline(buffer,256,' '); //Obtengo num filas
+    //    int nFilas = catoi(buffer);
+    //    
+    //    file.getline(buffer,256,' '); //Obtengo num colunmnas
+    //    int nColum = catoi(buffer);
+    //    
+    //    int matriz[nFilas+1][nColum]; //Añado una fila para el coste
+    //    
+    //    if (file.is_open()){
+    //        
+    //        while (!file.eof()){
+    //            
+    //            file.getline(buffer,256,' ');
+    //
+    //            string test = (string)buffer;
+    //
+    //            int num = stoi(test);
+    //
+    //            file.close();
+    //        }
+    //
+    //        
+    //    }
+
+    //A partir de aqui comienza el menú
+    int opcion = 0;
+    do {
+        cout << "Introduce 0 para algoritmo greedy " << endl;
+        cout << "Introduce 1 para algoritmo de búsqueda local" << endl;
+        cout << "Introduce 2 para algoritmo tabú" << endl;
+        cout << "Introduce 3 para algoritmo GRASP" << endl;
+        cout << "Introduce -1 para salir" << endl;
+        cin>>opcion;
+        switch (opcion) {
+            case 0:
+                // algoritmo greedy
+                break;
+
+            case 1:
+                //algoritmo de busqueda local
+                break;
+
+            case 2:
+                //algoritmo de busqueda tabu
+                break;
+            case 3:
+                //algoritmo GRASP
+                break;
+
+
+        }
+
+    } while (opcion != -1);
     Matriz prueba(4,5);
     prueba.insertar(2,3,5);
     cout << prueba.at(2,3);
-    
+    /*for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (j == 4)
+                cout << prueba.at(i,j) << endl;
+            else
+                cout << prueba.at(i,j);
+        }
+    }
+     * */
 
 
     return 0;
